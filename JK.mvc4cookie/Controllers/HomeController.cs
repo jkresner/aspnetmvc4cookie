efa6743c53bfd2c03c7bb121e96950ca5fd57c8c
Jonathon Kresner <jkresner@gmail.com>
2012-11-09 19:42:24 -0800

    Clean out default mvc4 template UI
    Replace with a little bit of bootstrap spice.

--- JK.mvc4cookie/Controllers/HomeController.cs
+++ JK.mvc4cookie/Controllers/HomeController.cs
@@ -10,21 +10,7 @@ namespace JK.mvc4cookie.Controllers
   {
     public ActionResult Index()
     {
-      ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-      return View();
-    }
-
-    public ActionResult About()
-    {
-      ViewBag.Message = "Your app description page.";
-
-      return View();
-    }
-
-    public ActionResult Contact()
-    {
-      ViewBag.Message = "Your contact page.";
+      ViewBag.Message = ViewBag.Message = "WELCOME TO MVC4 COOKIE.";
 
       return View();
     }
