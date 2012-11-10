19ab7ae0d50086eb9207ce3c62de3188b0f45810
Jonathon Kresner <jkresner@gmail.com>
2012-11-09 19:36:09 -0800

    Configure default site wide js/css bundles
    - Remove jquery UI
    - Fix up all directory paths
    - Default bundle for js
    (consolelog,placeholder,underscore,backbone,bootstrap & app namespace)
    - Default bundle for css (global and bootstrap)

--- JK.mvc4cookie/App_Start/BundleConfig.cs
+++ JK.mvc4cookie/App_Start/BundleConfig.cs
@@ -9,35 +9,29 @@ namespace JK.mvc4cookie
     public static void RegisterBundles(BundleCollection bundles)
     {
       bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                  "~/Scripts/jquery-{version}.js"));
-
-      bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                  "~/Scripts/jquery-ui-{version}.js"));
+                  "~/Content/js/libs/jquery-{version}.js"));
 
       bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                  "~/Scripts/jquery.unobtrusive*",
-                  "~/Scripts/jquery.validate*"));
+                  "~/Content/js/libs/jquery.unobtrusive*",
+                  "~/Content/js/libs/jquery.validate*"));
 
       // Use the development version of Modernizr to develop with and learn from. Then, when you're
       // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
       bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                  "~/Scripts/modernizr-*"));
+                  "~/Content/js/libs/modernizr-*"));
 
-      bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+      bundles.Add(new ScriptBundle("~/js/site").Include(
+        "~/Content/js/libs/consolelog.js",  //-- Cross browser js console.log
+        "~/Content/js/libs/placeholder.js",    //-- IE input placeholder text
+        "~/Content/js/libs/underscore.js",  //-- 'like' linq for js (set algebra functions)
+        "~/Content/js/libs/backbone.js",  //-- Client side 'mvc like' framework
+        "~/Content/js/libs/bootstrap.js",   //-- twitter bootstrap js extensions (modals/alerts and more)
+        "~/Content/js/JK.js")); //-- Our apps JS global namespace
 
-      bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                  "~/Content/themes/base/jquery.ui.core.css",
-                  "~/Content/themes/base/jquery.ui.resizable.css",
-                  "~/Content/themes/base/jquery.ui.selectable.css",
-                  "~/Content/themes/base/jquery.ui.accordion.css",
-                  "~/Content/themes/base/jquery.ui.autocomplete.css",
-                  "~/Content/themes/base/jquery.ui.button.css",
-                  "~/Content/themes/base/jquery.ui.dialog.css",
-                  "~/Content/themes/base/jquery.ui.slider.css",
-                  "~/Content/themes/base/jquery.ui.tabs.css",
-                  "~/Content/themes/base/jquery.ui.datepicker.css",
-                  "~/Content/themes/base/jquery.ui.progressbar.css",
-                  "~/Content/themes/base/jquery.ui.theme.css"));
+      bundles.Add(new StyleBundle("~/css/site").Include(
+        "~/Content/css/libs/bootstrap.css",
+        "~/Content/css/libs/bootstrap-responsive.css",
+        "~/Content/css/global.css"));
     }
   }
 }
